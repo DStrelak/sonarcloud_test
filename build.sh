b7143812dbebb1e1e9abd373c021fda0028c60e4
David Strelak <davidstrelak@gmail.com>
2019-02-07 15:55:53 +0100

    init commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "BUILDING"
+mkdir build
+
+g++ -Wall -o build/BiggestUnInt src/BiggestUnInt.cc
+g++ -Wall -o build/HelloWorld src/HelloWorld.cpp
+g++ -Wall -o build/SimpleClass src/SimpleClass.cc
+
+RC=$?
+
+if [ $RC -ne 0 ]; then
+  echo "BUILD FAILURE"
+else
+  echo "BUILD SUCCESS"
+fi
