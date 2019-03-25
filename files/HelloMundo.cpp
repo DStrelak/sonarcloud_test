9bb62219d5c8238be6edcaa1bd86de67074b68c7
David Strelak <davidstrelak@gmail.com>
2019-03-25 16:20:27 +0100

    adding obvious issues

--- files/HelloMundo.cpp
+++ files/HelloMundo.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main ()
 {
+   int* aaa = nullptr;
+   int bbb = *aaa;
+   int* ccc;
+   int ddd = *ccc;
+   if (bbb > ccc) return -1;   
    cout << "Hola tio, que tal?";
    return 0;
 }
